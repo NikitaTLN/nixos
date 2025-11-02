6b2340cebbf5958e02f16ec678e74d0249a52a5c
Nikita Ljubimov <nikitaljubimov@gmail.com>
2025-11-02 10:48:34 +0100

    hewo

--- home.nix
+++ home.nix
@@ -29,7 +29,7 @@ in
   services.walker.enable = true;
   programs.zen-browser.enable = true;
   programs.dankMaterialShell.enable = true;
-  programs.quickshell.enable = true;
+#  programs.quickshell.enable = true;
   home.username = "w1dget";
   home.homeDirectory = "/home/w1dget";
   home.stateVersion = "25.05";

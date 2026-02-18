7528e581589dc401b89916eb0c54d681c2bc7a13
Nikita Ljubimov <nikitaljubimov@gmail.com>
2026-02-18 21:00:26 +0100

    hewo

--- configuration.nix
+++ configuration.nix
@@ -13,9 +13,8 @@
   services.ratbagd.enable = true;
   i18n.defaultLocale = "ru_RU.UTF-8";
 
-  networking.firewall.allowedTCPPorts = [ 7000 7001 7100 ];
-  networking.firewall.allowedUDPPorts = [ 5353 6000 6001 7011 ];
 
+  networking.firewall.enable = false;
   services.avahi = {
       enable = true;
       nssmdns = true;
